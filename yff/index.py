2e3133831eb7edac5550bcd35b1f0650d2d5d001
David Kroll <dkroll713@gmail.com>
2024-08-22 18:59:57 -0500

    script should run fine now that it's checking time and running once

--- yff/index.py
+++ yff/index.py
@@ -16,10 +16,11 @@ def run_script():
 def schedule_next_run():
     # Get the current date and time
     now = datetime.now()
+    next_run_time = now + timedelta(minutes=1)
 
     # Calculate the next Wednesday 7:20 PM
-    next_wednesday = now + timedelta(days=(2 - now.weekday() + 7) % 7)
-    next_run_time = next_wednesday.replace(hour=19, minute=20, second=0, microsecond=0)
+    # next_wednesday = now + timedelta(days=(2 - now.weekday() + 7) % 7)
+    # next_run_time = next_wednesday.replace(hour=19, minute=20, second=0, microsecond=0)
 
     return next_run_time
 
@@ -27,10 +28,10 @@ if __name__ == "__main__":
     next_run_time = schedule_next_run()
     print('Next run time: ' + str(next_run_time))
     while True:
-    #     current_time = datetime.now()
+        current_time = datetime.now()
 
-    #     if current_time >= next_run_time:
+        if current_time >= next_run_time:
             run_script()
             next_run_time += timedelta(weeks=1)  # Schedule for the next Wednesday
-        # else:
-        #     time.sleep((next_run_time - current_time).total_seconds())
+        else:
+            time.sleep((next_run_time - current_time).total_seconds())
